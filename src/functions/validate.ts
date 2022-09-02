44940ad6b6b78a738a6098dac884d8bbd0df71bf
Ali AlQassab <ali@404-software.com>
2022-09-02 21:32:17 +0300

    Improve readability

--- src/functions/validate.ts
+++ src/functions/validate.ts
@@ -1,6 +1,6 @@
 import { validate as emailValidation } from 'email-validator'
 import ERRORS, { ApolloError } from './errors'
-const validatePhoneNumberNodeJs = require('validate-phone-number-node-js') as {
+const validatePhoneNumber = require('validate-phone-number-node-js') as {
 	validate: (phoneNumber: string) => boolean
 }
 
@@ -12,29 +12,32 @@ export const validatePassword = (password: string) => {
 
 	const tests = [/^(?=.*[A-Z])/, /^(?=.*[0-9])/, /^(?=.*[!@#$%^&*])/]
 
-	tests.forEach(test => {
-		if (!test.test(password)) valid = false
+	tests.forEach(({ test }) => {
+		if (!test(password)) valid = false
 	})
 
-	if (!valid)
-		throw ApolloError(
-			ERRORS.MALFORMED_INPUT,
-			'Password must have at least 8 characters, contain at least one uppercase letter, one number, and one special character',
-		)
+	if (valid) return
+
+	throw ApolloError(
+		ERRORS.MALFORMED_INPUT,
+		'Password must have at least 8 characters, contain at least one uppercase letter, one number, and one special character',
+	)
 }
 
 export const validateEmail = (email: string) => {
-	if (!emailValidation(email))
-		throw ApolloError(
-			ERRORS.MALFORMED_INPUT,
-			'Please provide a valid email address',
-		)
+	if (emailValidation(email)) return
+
+	throw ApolloError(
+		ERRORS.MALFORMED_INPUT,
+		'Please provide a valid email address',
+	)
 }
 
 export const validateMobile = (mobile: string) => {
-	if (!validatePhoneNumberNodeJs.validate(mobile))
-		throw ApolloError(
-			ERRORS.MALFORMED_INPUT,
-			'Please provide a valid mobile number (e.g. +97333123456)',
-		)
+	if (validatePhoneNumber.validate(mobile)) return
+
+	throw ApolloError(
+		ERRORS.MALFORMED_INPUT,
+		'Please provide a valid mobile number (e.g. +97333123456)',
+	)
 }
